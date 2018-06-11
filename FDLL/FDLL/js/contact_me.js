5290c8c0e07f51a1faaeca58be6ca141e7e9126b
Rachel Martin <rachel.s.martin@okstate.edu>
2018-06-11 16:06:51 -0400

    restructuring part 2

--- FDLL/FDLL/js/contact_me.js
+++ FDLL/FDLL/js/contact_me.js
@@ -35,7 +35,7 @@ $(function() {
           $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
           $('#success > .alert-success')
-            .append("<strong>Gracias, el mensaje envio </strong>");
+            .append("<strong>Your message has been sent. </strong>");
           $('#success > .alert-success')
             .append('</div>');
           //clear all fields
